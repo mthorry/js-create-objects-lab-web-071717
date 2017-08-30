520eb56cae5407a24deb28afe74e00e8c3194bfb
mthorry <mthorry@gmail.com>
2017-08-30 17:33:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 'use strict';
+
+function Sandwich(bread, crust, meat, condiments, veggies, cheese) {
+  this.breadType = bread;
+  this.crust = crust;
+  this.meat = meat;
+  this.condiments = condiments;
+  this.veggies = veggies;
+  this.cheese = cheese;
+}
+
+function Dog(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Ferret(name, age, foodRequirements) {
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+}
+
+function Fish(name, species, waterConditions) {
+  this.name = name;
+  this.species = species;
+  this.waterConditions = waterConditions;
+}
+
+function Bird(name, breed) {
+  this.name = name;
+  this.breed = breed;
+}
